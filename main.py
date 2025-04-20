c5afb1064732904d7222ace4ebdd8d70875c6655
Prathamesh <prathameshtulsankar0@gmail.com>
2025-04-20 21:25:47 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+import os
+import eel # type: ignore
+
+from engine.features import *
+from engine.command import *
+
+def start():
+    
+    eel.init("www")
+
+    playAssistantSound()
+
+
+    os.system('start brave.exe --app="http://localhost:8000/index.html"')
+
+    eel.start('index.html', mode=None, host='localhost', block=True)
