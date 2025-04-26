99ef1290faae9d6578944d29c9aa466ad806f3f1
Prathamesh <prathameshtulsankar0@gmail.com>
2025-04-26 11:08:37 +0530

    Add files via upload

---
+++ db.py
@@ -0,0 +1,50 @@
+import csv
+import sqlite3
+
+conn = sqlite3.connect("jarvis.db")
+cursor = conn.cursor()
+
+# query = "CREATE TABLE IF NOT EXISTS sys_command(id integer primary key, name VARCHAR(100), path VARCHAR(1000))"
+# cursor.execute(query)
+
+# query = "INSERT INTO sys_command VALUES (null,'star uml','C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StarUML.lnk')"
+# cursor.execute(query)
+# conn.commit()
+
+# query = "CREATE TABLE IF NOT EXISTS web_command(id integer primary key, name VARCHAR(100), url VARCHAR(1000))"
+# cursor.execute(query)
+
+# query = "INSERT INTO web_command VALUES (null,'geeks for geeks', 'https://www.geeksforgeeks.org/')"
+# cursor.execute(query)
+# conn.commit()
+
+# Create a table with the desired columns
+# cursor.execute('''CREATE TABLE IF NOT EXISTS contacts (id integer primary key, name VARCHAR(200), mobile_no VARCHAR(255), email VARCHAR(255) NULL)''')
+
+# Specify the column indices you want to import (0-based index)
+# Example: Importing the 1st and 3rd columns
+# desired_columns_indices = [0, 32]
+
+# # # Read data from CSV and insert into SQLite table for the desired columns
+# with open('contacts.csv', 'r', encoding='utf-8') as csvfile:
+#     csvreader = csv.reader(csvfile)
+#     for row in csvreader:
+#         selected_data = [row[i] for i in desired_columns_indices]
+#         cursor.execute(''' INSERT INTO contacts (id, 'name', 'mobile_no') VALUES (null, ?, ?);''', tuple(selected_data))
+
+# # # Commit changes and close connection
+# conn.commit()
+# conn.close()
+
+
+# inserting single contact
+query = "UPDATE contacts SET name='Priya' WHERE id=157"
+cursor.execute(query)
+conn.commit()
+
+# query = 'mom'
+# query = query.strip().lower()
+
+# cursor.execute("SELECT mobile_no FROM contacts WHERE LOWER(name) LIKE ? OR LOWER(name) LIKE ?", ('%' + query + '%', query + '%'))
+# results = cursor.fetchall()
+# print(results[0][0])
